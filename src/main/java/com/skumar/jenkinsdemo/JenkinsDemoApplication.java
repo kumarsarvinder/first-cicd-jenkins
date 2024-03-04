2fa068ac9b79544267bbe49e82a34c8f117c6cc0
kumarsarvinder@github.com <skumary200@outlook.com>
2024-03-04 21:17:57 +0530

    changes in pom

--- src/main/java/com/skumar/jenkinsdemo/JenkinsDemoApplication.java
+++ src/main/java/com/skumar/jenkinsdemo/JenkinsDemoApplication.java
@@ -2,6 +2,8 @@ package com.skumar.jenkinsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,8 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-//@PropertySource("file:application.properties")
-public class JenkinsDemoApplication {
+public class JenkinsDemoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JenkinsDemoApplication.class);
+	}
 
 	@GetMapping("/greetings/{name}")
 	public String getGreetings(@PathVariable String name){
